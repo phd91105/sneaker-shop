34f04da7fc6f0767123a626356dd29f7f2b18a5c
Pham Duy <phd051199@gmail.com>
2021-04-16 05:48:59 +0700

    update

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 define("DB_HOST", "127.0.0.1");
-define("DB_USER", "root");
-define("DB_PASS", "");
+define("DB_USER", "pi");
+define("DB_PASS", "Tdpamm00");
 define("DB_NAME", "sneaker_db");
 
 ?>
